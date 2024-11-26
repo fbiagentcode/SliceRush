dc36be16b6136d504ecac060599adc050319a87c
Chandana Ramesh <chandanaramesh516@gmail.com>
2024-11-26 23:00:17 +0530

    feat: add pre save hook and static to user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import hideEmail from "../utils/hideEmail.js";
+import bcrypt from "bcrypt";
 
 const USER_SCHEMA = new mongoose.Schema({
 	role: { type: String, enum: ["customer", "admin"], default: "customer" },
@@ -8,6 +9,7 @@ const USER_SCHEMA = new mongoose.Schema({
         type: String, 
         required: true, 
         unique: true, 
+        set: (v) => v.toUpperCase(),
         get: hideEmail, 
         validate: [(email) => validator.isEmail(email), "Invalid email format."]
     },
@@ -17,6 +19,15 @@ const USER_SCHEMA = new mongoose.Schema({
 	orders: [mongoose.SchemaTypes.ObjectId]
 });
 
-// USER_SCHEMA.pre("save", async 
+USER_SCHEMA.pre("save", async function(next){
+    if (this.isModified("password")) this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+USER_SCHEMA.statics.login = async function login(email, password){
+    const user = await this.findOne({email});
+    if (!user) return null;
+    return await bcrypt.compare(password, user.password) && user;
+};
 
 export default mongoose.model("User", USER_SCHEMA);
