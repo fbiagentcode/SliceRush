635ce364a3c7c3e03c4ad7edffa87dda0f0a61eb
Chandana Ramesh <chandanaramesh516@gmail.com>
2024-12-03 17:24:35 +0530

    add auth context provider

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useReducer, useEffect } from "react";
+export const authContext = createContext(null);
+
+export default function AuthContextProvider({children}){
+    const [ user, dispatch ] = useReducer(authReducer);
+
+    useEffect(() => {
+        if (!user){
+            // get user details from local storage
+            const userStorage = localStorage.getItem("auth");
+            if(user) dispatch({type: "LOGIN", payload: JSON.parse(userStorage)});
+        }
+    }, [])
+
+    useEffect(() => console.log("auth", user));
+
+    return <authContext.Provider value= { { user, dispatch } }>
+        {children}
+    </authContext.Provider>
+}
+
+function authReducer(state, {payload, type}){
+    switch (type){
+        case "LOGIN":
+            const user = { 
+                _id: payload._id, 
+                role: payload.role, 
+                name: payload.name, 
+                imageUrl: payload.imageUrl
+            };
+            localStorage.setItem("user", JSON.stringify(user));
+            return { _id, role, name, imageUrl };
+        case "LOGOUT":
+            localStorage.removeItem("user");
+            return null;
+        default: return state;
+    }
+}
