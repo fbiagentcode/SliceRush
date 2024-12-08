16914e6cc25345aa33940e2a13613837adecd601
Chandana Ramesh <chandanaramesh516@gmail.com>
2024-12-08 17:44:53 +0530

    feat: add navigation bar and fix log out issue
    fixed logouts not happening de to dispatch being wrongly called. also disabled strict mode

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import { useContext, useRef, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
+import useFetch from "../../hooks/useFetch";
+import { Button } from "../ui/button";
+import ButtonLoading from "../ui/ButtonLoading";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+import { authContext } from "../../contexts/AuthContext";
+
+const origin = import.meta.env.VITE_ORIGIN;
+
+export default function Navbar(){
+    const { fetchHandler, isLoading, error } = useFetch();
+    const controller = useRef();
+    const { user, dispatch } = useContext(authContext);
+
+    const handleLogout = async () => {
+        const result = await fetchHandler(`${origin}/auth/logout`, controller.current.signal);
+        if (result)
+            dispatch({type: "LOGOUT"});
+    };
+
+    useEffect(() => {
+        controller.current = new AbortController();
+
+        return () => controller.current.abort();
+    }, []);
+
+    return <NavigationMenu>
+        <NavigationMenuList>
+            <NavigationMenuItem>
+                <NavLink to= "/">Home</NavLink>
+            </NavigationMenuItem>
+            { user? <>
+                <NavigationMenuItem>
+                    <NavLink 
+                        
+                        to= { user.role === "customer"? "/dashboard" : "/admin-dashboard" }
+                    >
+                        Dashboard
+                    </NavLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    { isLoading? <ButtonLoading>Logging out...</ButtonLoading> :
+                    <Button 
+                        variant= "ghost" 
+                        onClick= {handleLogout}
+                    >
+                        Log Out
+                    </Button> }
+                </NavigationMenuItem>
+            </> : 
+            <NavigationMenuItem>
+                <NavLink to="/?login=true">Login</NavLink>
+            </NavigationMenuItem>
+            }
+        </NavigationMenuList>
+    </NavigationMenu>
+
+}
